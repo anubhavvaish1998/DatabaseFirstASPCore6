5385afed5d14b6d949e6701423b6d5ebffc7ae9f
Anubhav Vaish <anubhavvaish04@gmail.com>
2023-11-03 21:48:50 +0530

    first commit

---
+++ DatabaseFirstASPCore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabaseFirstASPCore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
